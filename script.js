8613fac9810f51ee01e894fe51bae139bf069382
Kari Meghana <karimeghana02@gmail.com>
2025-07-15 09:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function stringChop(str, size) {
-  // your code here
+function chunkString(str, chunkLength) {
+    if (str === null) {
+        return [];
+    }
+
+    let chunks = [];
+    for (let i = 0; i < str.length; i += chunkLength) {
+        chunks.push(str.substring(i, i + chunkLength));
+    }
+
+    return chunks;
 }
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+
