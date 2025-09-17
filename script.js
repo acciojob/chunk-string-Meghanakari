2247e9d5c353bdcbc443be6f5f53a452a9628db4
Kari Meghana <karimeghana02@gmail.com>
2025-09-17 21:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function chunkString(str,sixe) {
     let chunks = [];
-    for (let i = 0; i < str.length-1; i++) {
+    for (let i = 0; i < str.length; i++) {
         let subStr=str.slice(i,i+size);
         chunks.push(subStr);
     }
